5a002bfa591e88281c6251b84d08be5353448cb3
Adam Engesath <adam@thywordistrue.com>
2019-11-09 22:41:01 -0500

    bug fix with fedex state

--- lib/active_shipping/carriers/fedex.rb
+++ lib/active_shipping/carriers/fedex.rb
@@ -469,6 +469,7 @@ module ActiveShipping
           xml.StreetLines(location.address1) if location.address1
           xml.StreetLines(location.address2) if location.address2
           xml.City(location.city) if location.city
+          xml.StateOrProvinceCode(location.state) if location.state
           xml.PostalCode(location.postal_code)
           xml.CountryCode(location.country_code(:alpha2))
           xml.Residential(true) unless location.commercial?
